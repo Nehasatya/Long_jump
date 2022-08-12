71dfb6cedd4bdcb0a69089dea3137615a7088980
Neha <nehasatya02@gmail.com>
2022-08-12 21:07:01 +0530

    Long jump App

---
+++ app/src/main/java/com/example/long_jump/Score_Entry.java
@@ -0,0 +1,14 @@
+package com.example.long_jump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Score_Entry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_entry);
+    }
+}
